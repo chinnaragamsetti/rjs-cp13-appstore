ec9ba7d033af660731fefed34737623d09428d82
chinnaragamsetti <110217256+chinnaragamsetti@users.noreply.github.com>
2024-01-11 22:35:04 +0530

    Update index.js

--- src/components/AppItem/index.js
+++ src/components/AppItem/index.js
@@ -1,18 +1,16 @@
 // Write your code here
 
-
 import './index.css'
 
-
-const AppItem=props=>{
-    const {eachdetails}=prop 
-    const {appId,appName,imageUrl,category}=eachdetails
-    return(
-        <div className='appcontainer'>
-            <img src={imageUrl className='image'}/>
-            <p className='appname'>{appName}</p>
-        </div>
-    )
+const AppItem = props => {
+  const {eachdetails} = props
+  const {appName, imageUrl} = eachdetails
+  return (
+    <div className="appcontainer">
+      <img src={imageUrl} className="image" alt={appName} />
+      <p className="appname">{appName}</p>
+    </div>
+  )
 }
 
 export default AppItem
